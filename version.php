379be1623cd90b65ef3a62f7203fc05632c9ddf0
Johannes Burk <j.burk@joois.net>
2014-06-10 11:32:22 +0200

    build number changed to actual code version and version number: 1.2.1 (build 20131209)

--- version.php
+++ version.php
@@ -18,8 +18,8 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2014060901;
+$plugin->version  = 20131209;
 $plugin->requires = 2011080100;
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = 'Course Enrol Apply Plugin Version 1.2.1(build 20131209)';
+$plugin->release = 'Course Enrol Apply Plugin Version 1.2.1 (build 20131209)';
 $plugin->cron = 180;
