a38c978d5388294503ca948ae29df850d9234f19
Johannes Burk <j.burk@joois.net>
2014-07-10 20:34:04 +0200

    Revert "Revert "Merge branch 'dev-lang' into develop"" Another mistake -.- , everything was fine :) !
    This reverts commit 7486272bd76a50b6d974c6f49604a8f71f16c311.

--- lang/de/enrol_apply.php
+++ lang/de/enrol_apply.php
@@ -1,18 +1,25 @@
 <?php
- 
+/**
+ * *************************************************************************
+ * *                  Apply	Enrol   				                      **
+ * *************************************************************************
+ * @copyright   emeneo.com                                                **
+ * @link        emeneo.com                                                **
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later  **
+ * *************************************************************************
+ * ************************************************************************
+*/ 
 // The name of your plugin. Displayed on admin menus.
 $string['enrolname'] = 'Bestätigung der Kurseinschreibung';
 $string['pluginname'] = 'Bestätigung der Kurseinschreibung';
+$string['pluginname_desc'] = 'With this plugin users can apply to a course and a teacher have to comfirm before the user gets enroled.';
 
-$string['confirmmailsubject'] = 'Bestätigung des Mail-Betreffs';
-$string['confirmmailcontent'] = 'Bestätigung des Mail-Inhalts';
-$string['cancelmailsubject'] = 'Löschen des Mail-Betreffs';
-$string['cancelmailcontent'] = 'Löschen des Mail-Inhalts';
-$string['mailaddress'] = 'Mailadresse senden';
-$string['mailusername'] = 'Mailbenutzernamen senden';
-$string['mailpassword'] = 'Mailpasswort senden';
-$string['confirmmailcontent_desc'] = 'Bitte benutzen Sie die Spezialmarkierungen, um den gewünschten Mailinhalt zu ersetzen.<br>{Vorname}:Registrierungsname; {Inhalt}:Kursname';
-$string['cancelmailcontent_desc'] = 'Bitte benutzen Sie die Spezialmarkierungen, um den gewünschten Mailinhalt zu ersetzen.<br>{Vorname}:Registrierungsname; {Inhalt}:Kursname';
+$string['confirmmailsubject'] = 'E-Mail-Betreff für Bestätigung der Einschreibung';
+$string['confirmmailcontent'] = 'E-Mail für Bestätigung der Einschreibung';
+$string['cancelmailsubject'] = 'Mail-Betreff für Verwerfen der Einschreibung';
+$string['cancelmailcontent'] = 'Mail für Verwerfen der Einschreibung';
+$string['confirmmailcontent_desc'] = 'Bitte benutzen Sie die Spezialmarkierungen, um den gewünschten Mailinhalt zu ersetzen.<br>{firstname}:Registrierungsname; {content}:Kursname';
+$string['cancelmailcontent_desc'] = 'Bitte benutzen Sie die Spezialmarkierungen, um den gewünschten Mailinhalt zu ersetzen.<br>{firstname}:Registrierungsname; {content}:Kursname';
 
 $string['confirmusers'] = 'Einschreibung bestätigen';
 
@@ -22,19 +29,22 @@ $string['applyusermail'] = 'Email';
 $string['applydate'] = 'Einschreibungsdatum';
 $string['btnconfirm'] = 'Bestätigen';
 $string['btncancel'] = 'Abbruch';
-$string['enrolusers'] = 'Benutzer einschreiben';
+$string['enrolusers'] = 'Benutzer manuell einschreiben';
 
 $string['status'] = 'Bestätigung der Kurseinschreibung erlauben';
-$string['confirmenrol'] = 'Anwendung verwalten';
+$string['confirmenrol'] = 'Einschreibeanfragen bearbeiten';
 
 $string['apply:config'] = 'Einschreibungsbelegstellen anlegen';
 $string['apply:manage'] = 'Einschreibungsanfragen verwalten';
 $string['apply:unenrol'] = 'Benutzer aus dem Kurs entfernen';
 $string['apply:unenrolapply'] = 'Sich selbst aus dem Kurs entfernen';
  
-// Description of your plugin. Shown on the plugin's configuration screen.
-$string['description'] = '';
-$string['notification'] = '<b>Einschreibungsantrag wurde erfolgreich gesendet.</b>. <br/><br/>Sie werden via Mail informiert, sobald Ihre Einschreibung bestätigt wurde. Wenn Sie sich in andere Kurse einschreiben wollen, klicken Sie bitte den "Kurskatalog" im Hauptmenu an.';
- 
+$string['notification'] = '<b>Einschreibungsantrag wurde erfolgreich gesendet.</b>. <br/><br/>Sie werden via Mail informiert, sobald Ihre Einschreibung bestätigt wurde.';
+
+$string['sendmailtoteacher'] = 'Sende eine Hinweis-E-Mail an den Trainer';
+$string['mailtoteacher_suject'] = 'Neue Anfrage zur Einschreibung';
+$string['editdescription'] = 'Beschreibung anpassen';
+$string['applymanage'] = 'Manage enrolment applications';
+
 $string['status_desc'] = 'Kurszugriff für intern eingeschriebene Nutzer/innen erlauben.';
 ?>
