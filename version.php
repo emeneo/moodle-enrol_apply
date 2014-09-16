fe1a579d85391f7ba6fb6c0e46d003bcb4594d21
Flotter Totte <flottertotte@emeneo.com>
2014-09-16 18:36:29 +0800

    Update version.php

--- version.php
+++ version.php
@@ -18,7 +18,7 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2014091000;
+$plugin->version  = 2014091500;
 $plugin->requires = 2011080100;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = 'Course Enrol Apply Plugin Version 1.3.1 (build 2014091000)';
