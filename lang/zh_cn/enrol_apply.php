71149412505dac5f802aa58227710c4ad9c25f95
lainme <lainme993@gmail.com>
2015-03-24 20:41:33 +0800

    Chinese Simplified translation

---
+++ lang/zh_cn/enrol_apply.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * *************************************************************************
+ * *                  Apply	Enrol   				                      **
+ * *************************************************************************
+ * @copyright   emeneo.com                                                **
+ * @link        emeneo.com                                                **
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later  **
+ * *************************************************************************
+ * ************************************************************************
+*/ 
+// The name of your plug-in. Displayed on admin menus.
+$string['enrolname'] = '选课确认';
+$string['pluginname'] = '选课确认';
+$string['pluginname_desc'] = '用户可以通过这个插件申请选课。然后教师或者站点管理员需要批准选课请求以使选课成功。';
+
+$string['confirmmailsubject'] = '确认邮件的标题';
+$string['confirmmailcontent'] = '确认邮件的内容';
+$string['cancelmailsubject'] = '取消邮件的标题';
+$string['cancelmailcontent'] = '取消邮件的内容';
+$string['confirmmailcontent_desc'] = '请通过下列特殊标记来用Moodle中的数据取代邮件正文中内容。<br/>{firstname}: 用户名字; {content}: 课程名字; {lastname}: 用户姓氏; {username}: 用户的注册名';
+$string['cancelmailcontent_desc'] = '请通过下列特殊标记来用Moodle中的数据取代邮件正文中内容。<br/>{firstname}: 用户名字; {content}: 课程名字; {lastname}: 用户姓氏; {username}: 用户的注册名';
+
+$string['confirmusers'] = '确认选课';
+
+$string['coursename'] = '课程';
+$string['applyuser'] = '名/姓';
+$string['applyusermail'] = '电子邮件';
+$string['applydate'] = '选课日期';
+$string['btnconfirm'] = '确认';
+$string['btncancel'] = '取消';
+$string['enrolusers'] = '选课的用户';
+
+$string['status'] = '允许选课确认';
+$string['confirmenrol'] = '管理申请';
+
+$string['apply:config'] = '配置选课确认的实例';
+$string['apply:manage'] = '管理选课申请';
+$string['apply:unenrol'] = '从课程中取消用户的选课';
+$string['apply:unenrolapply'] = '从课程中取消自身的选课'; // is this necessary now?
+$string['apply:unenrolself'] = '从课程中取消自身的选课';
+
+ 
+$string['notification'] = '<b>选课申请已发送成功</b>。<br/><br/>申请通过后您会收到邮件通知。';
+
+$string['sendmailtoteacher'] = '向教师发送邮件通知';
+$string['sendmailtomanager'] = '向管理员发送邮件通知';
+$string['mailtoteacher_suject'] = '新的选课申请！';
+$string['editdescription'] = '文本区描述';
+$string['comment'] = '评论';
+$string['applymanage'] = '管理选课申请';
+
+$string['status_desc'] = '允许内部选课的用户进入课程';
+?>
