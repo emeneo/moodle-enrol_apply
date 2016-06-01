0211dbfd10fb416c653c064126f8b0f7bdfc8a92
Johannes Burk <me@jojoob.de>
2016-06-01 20:01:32 +0200

    Optimize apply_form

--- apply_form.php
+++ apply_form.php
@@ -20,13 +20,15 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-require_once("$CFG->libdir/formslib.php");
+require_once($CFG->libdir.'/formslib.php');
+require_once($CFG->dirroot.'/user/editlib.php');
+require_once($CFG->dirroot.'/user/profile/lib.php');
 
 class enrol_apply_apply_form extends moodleform {
     protected $instance;
 
     /**
-     * Overriding this function to get unique form id for multiple self enrolments
+     * Overriding this function to get unique form id for multiple apply enrolments
      *
      * @return string form identifier
      */
@@ -36,7 +38,8 @@ class enrol_apply_apply_form extends moodleform {
     }
 
     public function definition() {
-        global $DB;
+        global $USER;
+
         $mform = $this->_form;
         $instance = $this->_customdata;
         $this->instance = $instance;
@@ -47,33 +50,20 @@ class enrol_apply_apply_form extends moodleform {
 
         $mform->addElement('html', '<p>'.$instance->customtext1.'</p>');
         $mform->addElement('textarea', 'applydescription', get_string('comment', 'enrol_apply'),'cols="80"');
+        $mform->setType('applydescription', PARAM_TEXT);
 
         //user profile
-        global $USER,$CFG,$DB;
-        require_once($CFG->libdir.'/gdlib.php');
-        require_once($CFG->dirroot.'/user/edit_form.php');
-        require_once($CFG->dirroot.'/user/editlib.php');
-        require_once($CFG->dirroot.'/user/profile/lib.php');
-        require_once($CFG->dirroot.'/user/lib.php');
-
-        $user = $DB->get_record('user',array('id'=>$USER->id));
         $editoroptions = $filemanageroptions = null;
 
-        $apply_setting = $DB->get_records_sql("select name,value from ".$CFG->prefix."config_plugins where plugin='enrol_apply'");
-
         if($instance->customint1){
-            useredit_shared_definition($mform, $editoroptions, $filemanageroptions,$user);
+            useredit_shared_definition($mform, $editoroptions, $filemanageroptions, $USER);
         }
 
         if($instance->customint2){
-            profile_definition($mform, $user->id);
+            profile_definition($mform, $USER->id);
         }
 
-        $profile_default_values = $user;
-        if (is_object($profile_default_values)) {
-            $profile_default_values = (array)$profile_default_values;
-        }
-        $mform->setDefaults($profile_default_values);
+        $mform->setDefaults((array)$USER);
         
         $this->add_action_buttons(false, get_string('enrolme', 'enrol_self'));
 
@@ -84,18 +74,5 @@ class enrol_apply_apply_form extends moodleform {
         $mform->addElement('hidden', 'instance');
         $mform->setType('instance', PARAM_INT);
         $mform->setDefault('instance', $instance->id);
-
-        //$mform->addElement('html',"<script type='text/javascript' src='../../lib/jquery/jquery-1.10.2.min.js'></script>");
-        //$mform->addElement('html','<script>$(document).ready(function(){$(".collapsible-actions a").trigger("click");})</script>');
-        //$mform->addElement('html','<script type="text/javascript">$(document).ready(function(){setTimeout(function(){$(".collapseexpand").trigger("click");},3000)})</script>');
-    }
-
-    public function validation($data, $files) {
-        global $DB, $CFG;
-
-        $errors = parent::validation($data, $files);
-        $instance = $this->instance;
-
-        return $errors;
     }
 }
