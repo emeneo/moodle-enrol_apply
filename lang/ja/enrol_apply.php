51ac6b73a359b6a4ae7b71bd90b978d53ee7250e
Flotter Totte <flottertotte@emeneo.com>
2016-06-13 13:47:25 +0800

    Japanese language

---
+++ lang/ja/enrol_apply.php
@@ -0,0 +1,85 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'enrol_apply', language 'ja', branch 'MOODLE_30_STABLE'
+ *
+ * @package   enrol_apply
+ * @copyright 1999 onwards Martin Dougiamas  {@link http://moodle.com}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['apply:config'] = '登録申請インスタンスを設定する';
+$string['applydate'] = '登録日';
+$string['applymanage'] = '登録申請を管理する';
+$string['apply:manageapplications'] = '登録申請を管理する';
+$string['apply:unenrol'] = 'コースからユーザを登録解除する';
+$string['apply:unenrolself'] = 'コースから自分を登録解除する';
+$string['applyuser'] = '名/姓';
+$string['applyusermail'] = 'Eメール';
+$string['btncancel'] = 'キャンセルリクエスト';
+$string['btnconfirm'] = 'リクエストを承認する';
+$string['btnwait'] = 'ユーザを順番待ちリストに加える';
+$string['cancelmailcontent'] = 'キャンセルメールのコンテンツ';
+$string['cancelmailcontent_desc'] = 'メールコンテンツをMoodleのデータにより置換する場合に次の特別記号を使用してください。
+<br />
+{lastname} ユーザの姓、{firstname} ユーザの名、{username} ユーザ名、{content} コース名';
+$string['cancelmail_desc'] = '&nbsp;';
+$string['cancelmail_heading'] = 'キャンセルメール';
+$string['cancelmailsubject'] = 'キャンセルメールの件名';
+$string['cancelmailsubject_desc'] = '&nbsp;';
+$string['comment'] = 'コメント';
+$string['confirmenrol'] = '申請を管理する';
+$string['confirmmailcontent'] = '承認メールのコンテンツ';
+$string['confirmmailcontent_desc'] = 'メールコンテンツをMoodleのデータにより置換する場合に次の特別記号を使用してください。
+<br />
+{lastname} ユーザの姓、{firstname} ユーザの名、{username} ユーザ名、{content} コース名';
+$string['confirmmail_desc'] = '&nbsp;  ';
+$string['confirmmail_heading'] = '承認メール';
+$string['confirmmailsubject'] = '承認メールの件名';
+$string['confirmmailsubject_desc'] = '&nbsp;';
+$string['confirmusers'] = '登録承認';
+$string['confirmusers_desc'] = '灰色の行のユーザは順番待ちリストに登録されています。';
+$string['coursename'] = 'コース';
+$string['editdescription'] = 'テキストエリア説明';
+$string['enrolname'] = 'コース登録承認';
+$string['enrolusers'] = 'ユーザを登録する';
+$string['mailtoteacher_suject'] = '新しい登録リクエスト!';
+$string['notification'] = '<b>登録申請が正常に送信されました</b>。
+<br　/><br　/>
+あなたの登録が承認された場合、メール連絡されます。';
+$string['notify_desc'] = '新しい登録申請に関する通知を誰が受信するか設定します。';
+$string['notify_heading'] = '通知設定';
+$string['pluginname'] = 'コース登録承認';
+$string['pluginname_desc'] = 'このプラグインによりユーザはコースへの登録を申請することができます。ユーザが登録されるには教師またはサイトマネージャが登録を承認する必要があります。';
+$string['sendmailtomanager'] = 'マネージャにメール通知を送信する';
+$string['sendmailtoteacher'] = '教師にメール通知を送信する';
+$string['show_extra_user_profile'] = '登録画面に追加ユーザプロファイルフィールドを表示する';
+$string['show_standard_user_profile'] = '登録画面に標準ユーザプロファイルフィールドを表示する';
+$string['status'] = 'コース登録承認を許可する';
+$string['status_desc'] = '内部登録済みユーザのコースアクセスを許可します。';
+$string['user_profile'] = 'ユーザプロファイル';
+$string['waitmailcontent'] = '順番待ちリストメールコンテンツ';
+$string['waitmailcontent_desc'] = 'メールコンテンツをMoodleのデータにより置換する場合に次の特別記号を使用してください。
+<br />
+{lastname} ユーザの姓、{firstname} ユーザの名、{username} ユーザ名、{content} コース名';
+$string['waitmail_desc'] = '&nbsp; ';
+$string['waitmail_heading'] = '順番待ちリストメール';
+$string['waitmailsubject'] = '順番待ちリストメール件名';
+$string['waitmailsubject_desc'] = '&nbsp;';
