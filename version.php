791f77eda8ecde0f4d4d0584593b9c21ee4c4e0d
Flotter Totte <flottertotte@emeneo.com>
2020-01-07 03:15:17 +0800

    ready for 3.7

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2019040400;
+$plugin->version  = 2019120400;
 $plugin->requires = 2011080100;
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = 'Enrolment upon approval plugin Version 3.5-e';
+$plugin->release = 'Enrolment upon approval plugin Version 3.7-a';
 $plugin->component = 'enrol_apply';
