d6e9e1747102c1764121811246af355c8735ceaa
Esdras Caleb Oliveira Silva <esdrascaleb@gmail.com>
2024-02-28 17:22:08 -0300

    merged

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 
-$plugin->version  = 2023070100;
+$plugin->version  = 2023101100;
 $plugin->requires = 2011080100;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = 'Enrolment upon approval plugin Version 4.1-a';
